2ef1a0275b504da6496477f1edaeef55b9040042
viktor <victorchernyaew@gmail.com>
2018-08-23 12:17:37 +0400

    поменял структуру

--- src/Main.java
+++ src/Main.java
@@ -4,15 +4,13 @@
  * @Java2 homework Lesson-1
  * @link https://github.com/des1z
  */
-import Course.*;
-import Animals.*;
+import competitors.*;
+import course.*;
+
 
 public class Main {
     public static void main(String[] args) {
-        Course c = new Course();  // Создаем полосу препятствий
-        Team team = new Team(); // Создаем команду
-        c.doIt(team); // Просим команду пройти полосу
-        team.showResults(); // Показываем результаты
+        
 
     }
 
