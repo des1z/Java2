cc0b1bbc1a6b7274a774429bedeb1d95a30d8e3e
viktor <victorchernyaew@gmail.com>
2018-08-23 12:40:21 +0400

    add three courses

---
+++ src/course/Cross.java
@@ -0,0 +1,16 @@
+package course;
+
+import competitors.Competitor;
+
+public class Cross extends Obstacle {
+    private int dist;
+
+    public Cross(int dist) {
+        this.dist = dist;
+    }
+
+    @Override
+    public void doIt(Competitor competitor) {
+        competitor.run(dist);
+    }
+}
