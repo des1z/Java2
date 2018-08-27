4cd639e28d1db64cc223a40e22e7379f5693c7d5
viktor <victorchernyaew@gmail.com>
2018-08-27 13:01:41 +0400

    add matrix,wrong matrix, wrongchar matrix

--- src/main.java
+++ src/main.java
@@ -4,5 +4,30 @@
  * @Java2 homework Lesson-2
  * @link https://github.com/des1z
  */
+
+import exception.*;
+import Converter.*;
+
 public class main {
+    public static void main(String[] args) {
+        String[][] correctMatrix = {
+                {"7", "6", "2", "1"},
+                {"1", "4", "3", "4"},
+                {"7", "3", "0", "9"},
+                {"9", "7", "2", "7"}
+        };
+        String[][] wrongSizeMatrix = {
+                {"6", "7", "6", "6"},
+                {"3", "9"},
+                {"0", "7", "2", "6"},
+                {"0", "7", "2", "6"}
+        };
+        String[][] wrongChar = {
+                {"6", "7", "6", "6"},
+                {"3", "8", "", "6"},
+                {"9", "7", "2", "6"},
+                {"9", "7", "2", "6"}
+        };
+
+
 }
