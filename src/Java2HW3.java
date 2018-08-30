e5ecdf765ebc04c503a629b1829f207463cd9ea1
viktor <victorchernyaew@gmail.com>
2018-08-30 13:30:19 +0400

    add calculation words and calculate uniq words

--- src/Java2HW3.java
+++ src/Java2HW3.java
@@ -1,4 +1,4 @@
-import java.util.HashMap;
+import java.util.*;
 
 /**
  * @version 30.08.2018
@@ -10,6 +10,16 @@ public class Java2HW3 {
     public static void main(String[] args) {
         String[] words = {"Java", "C++", "C#", "Python", "Java", "Ruby", "Delphi", "PHP", "C++", "JavaScript"};
 
+        Map<String, Integer> map = new HashMap<>();
+        for (String word : words) {
+            map.put(word, map.getOrDefault(word, 0) + 1);
+        }
 
+        System.out.println(map);
+        System.out.println("");
+
+        Set<String> uniq = new HashSet<>(Arrays.asList(words));
+        System.out.println(uniq);
+        System.out.println("");
     }
 }
