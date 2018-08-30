1f61d2dab13a8f4aa353dcd3649bb6b74e81d6e8
viktor <victorchernyaew@gmail.com>
2018-08-30 13:18:35 +0400

    add array in class main and release map in phonebook

--- src/Java2HW3.java
+++ src/Java2HW3.java
@@ -1,3 +1,5 @@
+import java.util.HashMap;
+
 /**
  * @version 30.08.2018
  * @author Viktor Chernyaev
@@ -5,4 +7,9 @@
  * @link https://github.com/des1z
  */
 public class Java2HW3 {
+    public static void main(String[] args) {
+        String[] words = {"Java", "C++", "C#", "Python", "Java", "Ruby", "Delphi", "PHP", "C++", "JavaScript"};
+
+
+    }
 }
--- src/Phonebook.java
+++ src/Phonebook.java
@@ -1,2 +1,9 @@
+import java.util.*;
+
 public class Phonebook {
+    private Map<String, HashSet<String>> map;
+
+    Phonebook() {
+        this.map = new HashMap<>();
+    }
 }
