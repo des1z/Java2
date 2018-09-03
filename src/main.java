48ffb4222b65981fa53407e346ef112a2adaee8a
viktor <victorchernyaew@gmail.com>
2018-09-03 13:50:41 +0400

    add classes frame, sendpanel, textpanel

--- src/main.java
+++ src/main.java
@@ -1,3 +1,6 @@
+import javax.swing.*;
+import java.awt.*;
+
 /**
  * @version 03.09.2018
  * @author Viktor Chernyaev
@@ -5,4 +8,7 @@
  * @link https://github.com/des1z
  */
 public class main {
+    public static void main(String[] args) {
+        SwingUtilities.invokeLater(Frame::new);
+    }
 }
