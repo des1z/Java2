082a064af4669c7191bc5a3a55187b1611d52b6b
viktor <victorchernyaew@gmail.com>
2018-09-03 14:00:14 +0400

    release textpanel

--- src/TextPanel.java
+++ src/TextPanel.java
@@ -1,2 +1,29 @@
-public class TextPanel {
+import javax.swing.*;
+import javax.swing.border.Border;
+import javax.swing.border.CompoundBorder;
+import javax.swing.border.EmptyBorder;
+import java.awt.*;
+
+public class TextPanel extends JPanel {
+    private JTextArea textArea;
+
+    TextPanel(){
+        setLayout(new BoxLayout(this, BoxLayout.X_AXIS));
+
+        textArea = new JTextArea();
+        textArea.setLineWrap(true);
+        textArea.setEditable(false);
+
+        Border line = BorderFactory.createLineBorder(Color.WHITE);
+        Border empty = new EmptyBorder(10, 10, 10, 10);
+        CompoundBorder border = new CompoundBorder(line, empty);
+        textArea.setBorder(border);
+
+        JScrollPane jScrollPane = new JScrollPane(textArea);
+        add(jScrollPane, BorderLayout.CENTER);
+    }
+
+    JTextArea getTextArea(){
+        return textArea;
+    }
 }
