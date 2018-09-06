dea899326ae54e4c97206858bb8f2633a7cc357a
viktor <victorchernyaew@gmail.com>
2018-09-06 12:11:40 +0400

    add initial

---
+++ src/Java2HW5.java
@@ -0,0 +1,12 @@
+/**
+ * @version 06.09.2018
+ * @author Viktor Chernyaev
+ * @Java2 homework Lesson-5
+ * @link https://github.com/des1z
+ */
+
+public class Java2HW5 {
+    public static void main(String[] args) {
+
+    }
+}
