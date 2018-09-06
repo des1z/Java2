de9f3154ad3a938d59160386751348eb6bc9f90b
viktor <victorchernyaew@gmail.com>
2018-09-06 12:26:08 +0400

    add runOneThread

--- src/Java2HW5.java
+++ src/Java2HW5.java
@@ -6,7 +6,20 @@
  */
 
 public class Java2HW5 {
-    public static void main(String[] args) {
+    private static final int size = 10000000;
+    private static final int h = size / 2;
 
+    public float[] calculate(float[] arr) {
+        for (int i = 0; i < arr.length; i++)
+            arr[i] = (float) (arr[i] * Math.sin(0.2f + arr[i] / 5) * Math.cos(0.2f + arr[i] / 5) * Math.cos(0.4f + arr[i] / 2));
+        return arr;
+    }
+
+    public void runOneThread() {
+        float[] arr = new float[size];
+        for (int i = 0; i < arr.length; i++) arr[i] = 1.0f;
+        long a = System.currentTimeMillis();
+        calculate(arr);
+        System.out.println("One thread method ends with: " + (System.currentTimeMillis() - a));
     }
 }
